4e15d53b1eae3d5fbd3f789c5c49ff5f556719c7
Matthew Phillips <matthew@phillipsoft.com>
2021-08-04 06:09:25 -0400

    Use the new defaultView for shimming

--- test/shim.test.js
+++ test/shim.test.js
@@ -1,8 +1,20 @@
-import { domShimSymbol } from '../lib/mod.js';
+import { domShimSymbol } from '../lib/mod.js?props=document,Text';
 import { assertEquals } from './deps.js';
 
-Deno.test('Text can be extended', () => {
+Deno.test('Text can be extended from the defaultView', () => {
   const { Text } = self[domShimSymbol].document.defaultView;
+  class Mark extends Text {
+    get nodeType() {
+      return -1;
+    }
+  }
+
+  let mark = new Mark();
+  assertEquals(mark.data, '');
+});
+
+Deno.test('Text can be extended from the root', () => {
+  const { Text } = self[domShimSymbol];
   class Mark extends Text {
     get nodeType() {
       return -1;
